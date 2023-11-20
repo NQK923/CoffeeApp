3a9a62a70b47ffaf2335dd4c26f89f9d6b6719ac
nqk09 <nqk090203@gmail.com>
2023-11-21 06:35:56 +0700

    Add Excel export

--- DAL/AttendanceDAL.cs
+++ DAL/AttendanceDAL.cs
@@ -6,7 +6,7 @@ namespace DAL
 {
     public class AttendanceDAL
     {
-        private string connectionString = @"Data Source=LAPTOP-AUM0GEB8\VANHAI;Initial Catalog=Coffee;User ID=sa;Password=123456;TrustServerCertificate=true;";
+        private string connectionString = @"Data Source=DESKTOP-0HUV1DN\SQLEXPRESS;Initial Catalog=Coffee;User ID=sa;Password=123;TrustServerCertificate=true;";
         public void CreateAttendance(AttendanceDTO newAttendance)
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
